38c5571e37ecee7f4af26b58dd2a04148c078aee
Peter Cho <pcho51990@gmail.com>
2020-06-17 10:38:51 -0400

    testing cors

--- server.js
+++ server.js
@@ -14,17 +14,6 @@ const app = express();
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
