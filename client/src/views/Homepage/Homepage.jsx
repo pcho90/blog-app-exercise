5e8105912ab52cb01898034b2a8f99481aa64aca
Peter Cho <pcho51990@gmail.com>
2020-06-17 17:24:05 -0400

    loader

--- client/src/views/Homepage/Homepage.jsx
+++ client/src/views/Homepage/Homepage.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
-
-import { getPosts } from '../../services/posts';
+import Loader from 'react-loader-spinner';
 
 import './Homepage.css';
+import { getPosts } from '../../services/posts';
 import Layout from '../../components/Layout/Layout';
 import Post from '../../components/Post/Post';
 
 const Homepage = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       const response = await getPosts();
       setPosts(response);
-      console.log(response);
+      setLoading(false);
     };
     fetchData();
   }, []);
@@ -20,6 +21,15 @@ const Homepage = () => {
   return (
     <Layout>
       <div className='homepage'>
+        {loading && (
+          <Loader
+            type='Hearts'
+            color='#00BFFF'
+            height={100}
+            width={100}
+            timeout={20000}
+          />
+        )}
         {posts.map((post, idx) => (
           <Post key={idx} {...post} />
         ))}
